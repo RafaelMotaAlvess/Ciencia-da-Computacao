05df739ff86ce261c601e3c67ea9788f0479a9eb
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2024-02-29 21:28:37 -0300

    feat: calculando teorema de pitagoras com input

---
+++ 4 Semestre/Calculo Numerico/M1/Aula_01_29_02_2024/teste02.m
@@ -0,0 +1,12 @@
+% Testando o SCRIPT
+% Resolve o Teorema de Pitagoras
+
+clear; clc;
+
+a = input('Digite o valor do A: ');
+b = input('Digite o valor do B: ');
+
+c = sqrt(a^2 + b^2);
+
+disp('O valor da Hipotenusa é = ');
+disp(c)
